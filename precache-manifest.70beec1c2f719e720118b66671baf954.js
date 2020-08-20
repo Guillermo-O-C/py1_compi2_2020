b4a20b2a8458048e9b82d7f442ede059b42a1527
Guillermo <josuegorellanac@gmail.com>
2020-08-20 00:23:41 -0600

    Updates

---
+++ precache-manifest.70beec1c2f719e720118b66671baf954.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62dde0465f73096a284b6a58692fce79",
+    "url": "/Guillermo-O-C/py1_compi2_2020/index.html"
+  },
+  {
+    "revision": "06624834c05f74045369",
+    "url": "/Guillermo-O-C/py1_compi2_2020/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a8f1de2f8d72bc3fdf36",
+    "url": "/Guillermo-O-C/py1_compi2_2020/static/js/2.1127a32c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Guillermo-O-C/py1_compi2_2020/static/js/2.1127a32c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06624834c05f74045369",
+    "url": "/Guillermo-O-C/py1_compi2_2020/static/js/main.9c5b63b0.chunk.js"
+  },
+  {
+    "revision": "9619dd2644f25c9d7ddd",
+    "url": "/Guillermo-O-C/py1_compi2_2020/static/js/runtime-main.cdc30da8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Guillermo-O-C/py1_compi2_2020/static/media/logo.5d5d9eef.svg"
+  }
+]);
